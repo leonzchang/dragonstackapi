7c9efc830c095fbae0bac008db30bba687eae8ae
張立 <bellerophon00530@gmail.com>
2021-11-16 12:41:12 +0800

    set pg

--- bin/databaseConfiguration.ts
+++ bin/databaseConfiguration.ts
@@ -1,7 +1,7 @@
 export default {
-  user: 'Your postgres Username',
+  user: 'ezagkxsaoqixwe',
   host: 'localhost',
-  database: 'dragonstackdb',
-  password: 'Your postgres User Password',
+  database: 'db9aa90fdrn6kj',
+  password: 'a3f9f66fd8e6e1e8fca1f731a0b40609d5156d1e5deb8c92a1f79e831b9f15c7',
   port: 5432,
 };
