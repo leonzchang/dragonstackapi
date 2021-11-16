708d7d434e5598afcbee4f155f53a35a4f643c05
張立 <bellerophon00530@gmail.com>
2021-11-16 13:00:42 +0800

    pg url

--- bin/databaseConfiguration.ts
+++ bin/databaseConfiguration.ts
@@ -1,6 +1,6 @@
 export default {
   user: 'ezagkxsaoqixwe',
-  host: 'postgres://ezagkxsaoqixwe:a3f9f66fd8e6e1e8fca1f731a0b40609d5156d1e5deb8c92a1f79e831b9f15c7@ec2-52-54-237-144.compute-1.amazonaws.com:5432/db9aa90fdrn6kj',
+  host: 'postgres://ezagkxsaoqixwe:a3f9f66fd8e6e1e8fca1f731a0b40609d5156d1e5deb8c92a1f79e831b9f15c7@ec2-52-54-237-144.compute-1.amazonaws.com',
   database: 'db9aa90fdrn6kj',
   password: 'a3f9f66fd8e6e1e8fca1f731a0b40609d5156d1e5deb8c92a1f79e831b9f15c7',
   port: 5432,
