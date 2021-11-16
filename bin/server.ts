c51957c19442bf1a9735ff796d4cfa074366b281
張立 <bellerophon00530@gmail.com>
2021-11-16 13:51:46 +0800

    port

--- bin/server.ts
+++ bin/server.ts
@@ -2,4 +2,4 @@ import app from '../app/index';
 
 const port = 3000;
 
-app.listen(port, () => console.log(`listening on port ${port}`));
+app.listen(process.env.PORT || port, () => console.log(`listening on port ${port}`));
